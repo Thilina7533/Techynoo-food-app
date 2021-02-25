633739365586a11fd9111167de45abe0f4ac5e59
Thilina7533 <ourvillage.td@gmail.com>
2021-02-25 22:05:01 +0530

    first commit

---
+++ techynoo/lib/utils/custom_functions.dart
@@ -0,0 +1,6 @@
+// import 'package:intl/intl.dart';
+
+// String formatCurrency(String Name){
+//   final formatCurrency = new NumberFormat.simpleCurrency();
+//   return formatCurrency.format(Name);
+// }
